4dc1f53df3e2e16f1f09cd3cb29cbf834f347d2f
kyrapaim <kyrapaim@ufl.edu>
2026-02-21 13:31:23 -0500

    Atualizando lexer.py com palavras-chave em português

--- lexer.py
+++ lexer.py
@@ -1,20 +1,256 @@
-# Updated lexer.py with Portuguese keywords
+importar re
 
-# Replacing keywords with Portuguese equivalents
+de enumeracao importar Enumeracao, auto
 
-# This is a placeholder since the actual content of lexer.py is needed to perform the replacement effectively.
+class TipoToken(Enumeracao):
+    # Palavras-chave - Português
+    SE = auto()           # se
+    SENAO = auto()        # senao
+    ENQUANTO = auto()     # enquanto
+    PARA = auto()         # para
+    FUNCAO = auto()       # função/def
+    RETORNA = auto()      # retorna
+    VERDADEIRO = auto()   # Verdadeiro
+    FALSO = auto()        # Falso
+    NULO = auto()         # Nulo
+    ESCREVA = auto()      # escreva
+    DEIXE = auto()        # deixe
+    
+    # Operadores
+    IGUAL = auto()        # =
+    MAIS = auto()         # +
+    MENOS = auto()        # -
+    MULT = auto()         # *
+    DIV = auto()          # /
+    MODULO = auto()       # %
+    IGUAL_IGUAL = auto()  # ==
+    DIFERENTE = auto()    # !=
+    MAIOR = auto()        # >
+    MENOR = auto()        # <
+    MAIOR_IGUAL = auto()  # >=
+    MENOR_IGUAL = auto()  # <=
+    
+    # Literais
+    NUMERO = auto()
+    TEXTO = auto()
+    IDENTIFICADOR = auto()
+    
+    # Delimitadores
+    LPAREN = auto()       # (
+    RPAREN = auto()       # )
+    LBRACE = auto()       # {
+    RBRACE = auto()       # }
+    VIRG = auto()         # ,
+    DOIS_PONTOS = auto()  # :
+    PONTO_VIRG = auto()   # ;
+    
+    EOF = auto()
 
-# Example replacements:
-# class -> classe
-# def -> funcao
-# if -> se
-# else -> senao
-# while -> enquanto
-# for -> para
-# import -> em
-# return -> retorna
-# try -> tente
-# except -> exceto
-# as -> como
-
-# Original lexer.py content would be manipulated here and replaced accordingly.
+class Lexer:
+    def __init__(self, codigo, idioma='pt'):
+        self.codigo = codigo
+        self.idioma = idioma  # 'pt' para Português, 'es' para Espanhol
+        self.pos = 0
+        self.linha = 1
+        self.coluna = 1
+        self.tokens = []
+        
+        # Define palavras-chave para ambos idiomas
+        self.palavras_chave = {
+            'pt': {
+                'se': TipoToken.SE,
+                'senao': TipoToken.SENAO,
+                'senão': TipoToken.SENAO,
+                'enquanto': TipoToken.ENQUANTO,
+                'para': TipoToken.PARA,
+                'função': TipoToken.FUNCAO,
+                'funcao': TipoToken.FUNCAO,
+                'retorna': TipoToken.RETORNA,
+                'verdadeiro': TipoToken.VERDADEIRO,
+                'falso': TipoToken.FALSO,
+                'nulo': TipoToken.NULO,
+                'escreva': TipoToken.ESCREVA,
+                'deixe': TipoToken.DEIXE,
+            },
+            'es': {
+                'si': TipoToken.SE,
+                'sino': TipoToken.SENAO,
+                'mientras': TipoToken.ENQUANTO,
+                'para': TipoToken.PARA,
+                'función': TipoToken.FUNCAO,
+                'funcion': TipoToken.FUNCAO,
+                'retorna': TipoToken.RETORNA,
+                'verdadero': TipoToken.VERDADEIRO,
+                'falso': TipoToken.FALSO,
+                'nulo': TipoToken.NULO,
+                'escribe': TipoToken.ESCREVA,
+                'deja': TipoToken.DEIXE,
+            }
+        }
+    
+    def tokenizar(self):
+        enquanto self.pos < len(self.codigo):
+            self._pular_espacos_brancos()
+            
+            se self.pos >= len(self.codigo):
+                quebra
+            
+            char = self.codigo[self.pos]
+            
+            # Tratar comentários
+            se char == '#':
+                self._pular_comentario()
+                continua
+            
+            # Tratar strings
+            se char em ('"', "'"):
+                self._ler_string()
+            # Tratar números
+            senao_se char.isdigit():
+                self._ler_numero()
+            # Tratar identificadores e palavras-chave
+            senao_se char.isalpha() ou char == '_':
+                self._ler_identificador()
+            # Tratar operadores e delimitadores
+            senao:
+                self._ler_operador()
+    
+    def _pular_espacos_brancos(self):
+        enquanto self.pos < len(self.codigo) e self.codigo[self.pos].isspace():
+            se self.codigo[self.pos] == '\n':
+                self.linha += 1
+                self.coluna = 1
+            senao:
+                self.coluna += 1
+            self.pos += 1
+    
+    def _pular_comentario(self):
+        enquanto self.pos < len(self.codigo) e self.codigo[self.pos] != '\n':
+            self.pos += 1
+    
+    def _ler_string(self):
+        aspas = self.codigo[self.pos]
+        self.pos += 1
+        valor = ""
+        
+        enquanto self.pos < len(self.codigo) e self.codigo[self.pos] != aspas:
+            se self.codigo[self.pos] == '\\':
+                self.pos += 1
+                se self.pos < len(self.codigo):
+                    valor += self.codigo[self.pos]
+                    self.pos += 1
+            senao:
+                valor += self.codigo[self.pos]
+                self.pos += 1
+        
+        se self.pos < len(self.codigo):
+            self.pos += 1
+        
+        self.tokens.append({
+            'tipo': TipoToken.TEXTO,
+            'valor': valor,
+            'linha': self.linha,
+            'coluna': self.coluna
+        })
+    
+    def _ler_numero(self):
+        valor = ""
+        enquanto self.pos < len(self.codigo) e (self.codigo[self.pos].isdigit() ou self.codigo[self.pos] == '.'): 
+            valor += self.codigo[self.pos]
+            self.pos += 1
+        
+        self.tokens.append({
+            'tipo': TipoToken.NUMERO,
+            'valor': float(valor) se '.' em valor senao int(valor),
+            'linha': self.linha,
+            'coluna': self.coluna
+        })
+    
+    def _ler_identificador(self):
+        valor = ""
+        enquanto self.pos < len(self.codigo) e (self.codigo[self.pos].isalnum() ou self.codigo[self.pos] == '_'):
+            valor += self.codigo[self.pos]
+            self.pos += 1
+        
+        palavras = self.palavras_chave[self.idioma]
+        tipo = palavras.get(valor.lower(), TipoToken.IDENTIFICADOR)
+        
+        self.tokens.append({
+            'tipo': tipo,
+            'valor': valor,
+            'linha': self.linha,
+            'coluna': self.coluna
+        })
+    
+    def _ler_operador(self):
+        char = self.codigo[self.pos]
+        proximo = self.codigo[self.pos + 1] se self.pos + 1 < len(self.codigo) senao ""
+        
+        dois_caracteres = char + proximo
+        
+        se dois_caracteres == "==":
+            self.tokens.append({'tipo': TipoToken.IGUAL_IGUAL, 'valor': '==', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 2
+        senao_se dois_caracteres == "!=":
+            self.tokens.append({'tipo': TipoToken.DIFERENTE, 'valor': '!=', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 2
+        senao_se dois_caracteres == ">=":
+            self.tokens.append({'tipo': TipoToken.MAIOR_IGUAL, 'valor': '>=', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 2
+        senao_se dois_caracteres == "<=":
+            self.tokens.append({'tipo': TipoToken.MENOR_IGUAL, 'valor': '<=', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 2
+        senao_se char == '=':
+            self.tokens.append({'tipo': TipoToken.IGUAL, 'valor': '=', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '+':
+            self.tokens.append({'tipo': TipoToken.MAIS, 'valor': '+', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '-':
+            self.tokens.append({'tipo': TipoToken.MENOS, 'valor': '-', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '*':
+            self.tokens.append({'tipo': TipoToken.MULT, 'valor': '*', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '/':
+            self.tokens.append({'tipo': TipoToken.DIV, 'valor': '/', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '%':
+            self.tokens.append({'tipo': TipoToken.MODULO, 'valor': '%', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '>':
+            self.tokens.append({'tipo': TipoToken.MAIOR, 'valor': '>', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '<':
+            self.tokens.append({'tipo': TipoToken.MENOR, 'valor': '<', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '(': 
+            self.tokens.append({'tipo': TipoToken.LPAREN, 'valor': '(', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == ')':
+            self.tokens.append({'tipo': TipoToken.RPAREN, 'valor': ')', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '{':
+            self.tokens.append({'tipo': TipoToken.LBRACE, 'valor': '{', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == '}':
+            self.tokens.append({'tipo': TipoToken.RBRACE, 'valor': '}', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == ',':
+            self.tokens.append({'tipo': TipoToken.VIRG, 'valor': ',', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == ':':
+            self.tokens.append({'tipo': TipoToken.DOIS_PONTOS, 'valor': ':', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao_se char == ';':
+            self.tokens.append({'tipo': TipoToken.PONTO_VIRG, 'valor': ';', 'linha': self.linha, 'coluna': self.coluna})
+            self.pos += 1
+        senao:
+            self.pos += 1
+        
+        self.coluna += 1
+    
+    def obter_tokens(self):
+        self.tokenizar()
+        self.tokens.append({'tipo': TipoToken.EOF, 'valor': '', 'linha': self.linha, 'coluna': self.coluna})
+        retorna self.tokens
