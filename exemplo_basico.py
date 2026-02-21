02dfb1a8a4450b403586a9aa30c47d8fe04d319f
kyrapaim <kyrapaim@ufl.edu>
2026-02-21 10:48:39 -0500

    Adiciona exemplo básico de uso do compilador Braython

---
+++ exemplo_basico.py
@@ -0,0 +1,17 @@
+# Exemplo básico de uso do compilador Braython
+
+# Impressão de texto
+print("Olá, Mundo!")
+
+# Atribuição de variável
+x = 10
+
+# Operações aritméticas
+y = x + 5
+z = x * 2
+
+# Condicional básica
+if y > z:
+    print("y é maior que z")
+else:
+    print("z é maior ou igual a y")
